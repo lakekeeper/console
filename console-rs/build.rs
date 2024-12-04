6183d7628b329cea4ec2a89d558ba8a895ab4556
Christian Thiel <christian@hansetag.com>
2024-12-04 18:11:52 +0100

    add npm install

--- console-rs/build.rs
+++ console-rs/build.rs
@@ -12,6 +12,11 @@ fn main() {
     fs::create_dir_all(&dest_path).unwrap();
     // Build the console (npm)
     std::process::Command::new("npm")
+        .args(&["install"])
+        .current_dir(repo_dir)
+        .status()
+        .expect("Failed to install Lakekeeper UI dependencies with npm");
+    std::process::Command::new("npm")
         .args(&["run", "build-placeholder"])
         .current_dir(repo_dir)
         .status()
