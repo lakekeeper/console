283644d11a66f284d7915ffa83a1f2dfa760b00f
Viktor Kessler <vikkes@gmail.com>
2024-12-10 13:34:19 +0100

    fix: refresh tab after delete (#47)

--- src/plugins/functions.ts
+++ src/plugins/functions.ts
@@ -763,10 +763,10 @@ async function dropView(
   warehouseId: string,
   namespacePath: string,
   viewName: string
-): Promise<boolean> {
+) {
   try {
     const client = ice.client;
-    const { error } = await ice.dropView({
+    const { data, error } = await ice.dropView({
       client,
       path: {
         prefix: warehouseId,
@@ -774,9 +774,10 @@ async function dropView(
         view: viewName,
       },
     });
+
     if (error) throw error;
 
-    return true;
+    return data;
   } catch (error: any) {
     handleError(error, new Error());
     throw error;
