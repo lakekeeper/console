8880ebc7c3d3c5d2d00fac86c70f405e10a346c9
Tobias Puetz <tobias@hansetag.com>
2024-12-12 11:28:50 +0100

    fix config mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,18 +2,41 @@ import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import pluginVue from 'eslint-plugin-vue';
-import prettier from 'eslint-config-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
 import pluginPrettier from 'eslint-plugin-prettier';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ['**/*.{js,mjs,cjs,ts,vue}'] },
-  { files: ['**/*.js'], languageOptions: { sourceType: 'script' } },
-  { languageOptions: { globals: globals.browser } },
+  eslintConfigPrettier,
+  {
+    files: ['**/*.{js,mjs,cjs,ts,vue}'],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    plugins: {
+      prettier: pluginPrettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'script',
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   ...pluginVue.configs['flat/essential'],
-  { files: ['**/*.vue'], languageOptions: { parserOptions: { parser: tseslint.parser } } },
+  {
+    files: ['**/*.vue'],
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+      },
+    },
+  },
   {
     ignores: [
       '*.gen.ts',
@@ -43,11 +66,8 @@ export default [
       '*.pdb',
       'rust-project.json',
       '.env.azure',
+      'console-rs',
     ],
-  },
-  {
-    files: ['**/*.{js,mjs,cjs,ts,vue}'],
-    extends: ['plugin:prettier/recommended'],
   },
   {
     files: ['**/*.ts', '**/*.vue'],
