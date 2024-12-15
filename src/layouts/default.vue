60d8b3a6cbeb9dd770fb34447d7c2401bfd9c713
Viktor <vikkes@gmail.com>
2024-12-15 21:48:05 +0100

    feat: warning if authentication is off

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -2,6 +2,7 @@
   <v-app>
     <AppBar v-if="visual.showAppOrNavBar" />
     <v-main>
+      <AuthorizationOffBanner v-if="!enabledAuthorization"></AuthorizationOffBanner>
       <NavigationBar v-if="visual.showAppOrNavBar" />
       <router-view />
     </v-main>
@@ -13,6 +14,8 @@
 <script lang="ts" setup>
 //
 
+import AuthorizationOffBanner from '@/components/AuthenticationDisabledWarningBanner.vue';
+import { enabledAuthorization } from '@/app.config';
 import { useVisualStore } from '@/stores/visual';
 const visual = useVisualStore();
 </script>
