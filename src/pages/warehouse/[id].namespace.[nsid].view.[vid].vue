06652a533e25310cdc45eca103f07c4ec7083312
Viktor <vikkes@gmail.com>
2025-09-23 15:56:03 +0200

    wip

--- src/pages/warehouse/[id].namespace.[nsid].view.[vid].vue
+++ src/pages/warehouse/[id].namespace.[nsid].view.[vid].vue
@@ -27,6 +27,7 @@
         <v-tabs v-model="tab">
           <v-tab value="overview" @click="loadTabData">overview</v-tab>
           <v-tab value="raw" @click="loadTabData">raw</v-tab>
+          <v-tab value="details" @click="loadTabData">details</v-tab>
           <v-tab
             v-if="enabledAuthentication && enabledPermissions"
             value="permissions"
@@ -75,9 +76,18 @@
                   size="small"
                   variant="outlined"
                   color="success"
+                  class="mr-8"
                   @click="depthRawRepresentation = depthRawRepresentationMax"
                   append-icon="mdi-expand-all">
                   Expand
+                </v-btn>
+                <v-btn
+                  size="small"
+                  variant="outlined"
+                  color="primary"
+                  @click="copyViewJson"
+                  prepend-icon="mdi-content-copy">
+                  Copy JSON
                 </v-btn>
               </div>
               <div style="height: 65vh; overflow: auto">
@@ -89,6 +99,234 @@
                   :virtual="false" />
               </div>
             </v-tabs-window-item>
+
+            <v-tabs-window-item value="details">
+              <v-card-text>
+                <v-row>
+                  <v-col cols="12" md="6">
+                    <v-card variant="outlined" class="mb-4">
+                      <v-card-title class="d-flex align-center">
+                        <v-icon class="mr-2">mdi-information-outline</v-icon>
+                        View Information
+                      </v-card-title>
+                      <v-divider></v-divider>
+                      <v-list density="compact">
+                        <v-list-item>
+                          <v-list-item-title>View UUID</v-list-item-title>
+                          <v-list-item-subtitle class="d-flex align-center">
+                            <span class="mr-2 font-mono">{{ view.metadata['view-uuid'] }}</span>
+                            <v-btn
+                              icon="mdi-content-copy"
+                              size="small"
+                              variant="flat"
+                              @click="
+                                functions.copyToClipboard(view.metadata['view-uuid'])
+                              "></v-btn>
+                          </v-list-item-subtitle>
+                        </v-list-item>
+
+                        <v-list-item>
+                          <v-list-item-title>Format Version</v-list-item-title>
+                          <v-list-item-subtitle>
+                            {{ view.metadata['format-version'] }}
+                          </v-list-item-subtitle>
+                        </v-list-item>
+
+                        <v-list-item v-if="view.metadata.location">
+                          <v-list-item-title>Location</v-list-item-title>
+                          <v-list-item-subtitle class="d-flex align-center">
+                            <span class="mr-2 font-mono text-wrap">
+                              {{ view.metadata.location }}
+                            </span>
+                            <v-btn
+                              icon="mdi-content-copy"
+                              size="small"
+                              variant="flat"
+                              @click="functions.copyToClipboard(view.metadata.location)"></v-btn>
+                          </v-list-item-subtitle>
+                        </v-list-item>
+
+                        <v-list-item v-if="view['metadata-location']">
+                          <v-list-item-title>Metadata Location</v-list-item-title>
+                          <v-list-item-subtitle class="d-flex align-center">
+                            <span class="mr-2 font-mono text-wrap">
+                              {{ view['metadata-location'] }}
+                            </span>
+                            <v-btn
+                              icon="mdi-content-copy"
+                              size="small"
+                              variant="flat"
+                              @click="functions.copyToClipboard(view['metadata-location'])"></v-btn>
+                          </v-list-item-subtitle>
+                        </v-list-item>
+
+                        <v-list-item v-if="view.metadata['current-version-id']">
+                          <v-list-item-title>Current Version ID</v-list-item-title>
+                          <v-list-item-subtitle>
+                            {{ view.metadata['current-version-id'] }}
+                          </v-list-item-subtitle>
+                        </v-list-item>
+                      </v-list>
+                    </v-card>
+                  </v-col>
+
+                  <v-col cols="12" md="6">
+                    <v-card variant="outlined" class="mb-4">
+                      <v-card-title class="d-flex align-center">
+                        <v-icon class="mr-2">mdi-table-cog</v-icon>
+                        Schema Information
+                      </v-card-title>
+                      <v-divider></v-divider>
+                      <v-list density="compact">
+                        <v-list-item
+                          v-if="view.metadata.schemas && view.metadata.schemas.length > 0">
+                          <v-list-item-title>Total Schemas</v-list-item-title>
+                          <v-list-item-subtitle>
+                            {{ view.metadata.schemas.length }}
+                          </v-list-item-subtitle>
+                        </v-list-item>
+
+                        <v-list-item v-if="currentViewSchema">
+                          <v-list-item-title>Current Schema Fields</v-list-item-title>
+                          <v-list-item-subtitle>
+                            {{ currentViewSchema?.fields?.length || 0 }}
+                          </v-list-item-subtitle>
+                        </v-list-item>
+
+                        <v-list-item
+                          v-if="view.metadata.versions && view.metadata.versions.length > 0">
+                          <v-list-item-title>Total Versions</v-list-item-title>
+                          <v-list-item-subtitle>
+                            {{ view.metadata.versions.length }}
+                          </v-list-item-subtitle>
+                        </v-list-item>
+
+                        <v-list-item v-if="currentViewVersion">
+                          <v-list-item-title>Current Schema ID</v-list-item-title>
+                          <v-list-item-subtitle>
+                            {{ currentViewVersion['schema-id'] }}
+                          </v-list-item-subtitle>
+                        </v-list-item>
+                      </v-list>
+                    </v-card>
+                  </v-col>
+                </v-row>
+
+                <!-- Properties Section -->
+                <v-row
+                  v-if="
+                    view.metadata.properties && Object.keys(view.metadata.properties).length > 0
+                  ">
+                  <v-col cols="12">
+                    <v-card variant="outlined" class="mb-4">
+                      <v-card-title class="d-flex align-center">
+                        <v-icon class="mr-2">mdi-cog-outline</v-icon>
+                        View Properties
+                      </v-card-title>
+                      <v-divider></v-divider>
+                      <v-card-text>
+                        <v-row>
+                          <v-col
+                            v-for="(value, key) in view.metadata.properties"
+                            :key="key"
+                            cols="12"
+                            md="6">
+                            <v-list-item class="pa-0">
+                              <v-list-item-title class="text-body-2 font-weight-medium">
+                                {{ key }}
+                              </v-list-item-title>
+                              <v-list-item-subtitle class="font-mono text-wrap">
+                                {{ value }}
+                              </v-list-item-subtitle>
+                            </v-list-item>
+                          </v-col>
+                        </v-row>
+                      </v-card-text>
+                    </v-card>
+                  </v-col>
+                </v-row>
+
+                <!-- Current Version Details -->
+                <v-row v-if="currentViewVersion">
+                  <v-col cols="12">
+                    <v-card variant="outlined">
+                      <v-card-title class="d-flex align-center">
+                        <v-icon class="mr-2">mdi-eye-outline</v-icon>
+                        Current Version Details
+                      </v-card-title>
+                      <v-divider></v-divider>
+                      <v-list density="compact">
+                        <v-list-item>
+                          <v-list-item-title>Version ID</v-list-item-title>
+                          <v-list-item-subtitle class="d-flex align-center">
+                            <span class="mr-2 font-mono">
+                              {{ currentViewVersion['version-id'] }}
+                            </span>
+                            <v-btn
+                              icon="mdi-content-copy"
+                              size="small"
+                              variant="flat"
+                              @click="
+                                functions.copyToClipboard(String(currentViewVersion['version-id']))
+                              "></v-btn>
+                          </v-list-item-subtitle>
+                        </v-list-item>
+
+                        <v-list-item v-if="currentViewVersion['timestamp-ms']">
+                          <v-list-item-title>Created</v-list-item-title>
+                          <v-list-item-subtitle>
+                            {{ formatTimestamp(currentViewVersion['timestamp-ms']) }}
+                          </v-list-item-subtitle>
+                        </v-list-item>
+
+                        <v-list-item
+                          v-if="
+                            currentViewVersion.representations &&
+                            currentViewVersion.representations[0]
+                          ">
+                          <v-list-item-title>SQL Query</v-list-item-title>
+                          <v-list-item-subtitle class="d-flex align-center">
+                            <span class="mr-2 font-mono text-wrap">
+                              {{ currentViewVersion.representations[0].sql }}
+                            </span>
+                            <v-btn
+                              icon="mdi-content-copy"
+                              size="small"
+                              variant="flat"
+                              @click="
+                                functions.copyToClipboard(currentViewVersion.representations[0].sql)
+                              "></v-btn>
+                          </v-list-item-subtitle>
+                        </v-list-item>
+
+                        <v-list-item v-if="currentViewVersion.summary">
+                          <v-list-item-title>Summary</v-list-item-title>
+                          <v-list-item-subtitle>
+                            <v-row>
+                              <v-col
+                                v-for="(value, key) in currentViewVersion.summary"
+                                :key="key"
+                                cols="12"
+                                md="6">
+                                <v-list-item class="pa-0" density="compact">
+                                  <v-list-item-title class="text-caption">
+                                    {{ key }}
+                                  </v-list-item-title>
+                                  <v-list-item-subtitle class="font-mono">
+                                    {{ value }}
+                                  </v-list-item-subtitle>
+                                </v-list-item>
+                              </v-col>
+                            </v-row>
+                          </v-list-item-subtitle>
+                        </v-list-item>
+                      </v-list>
+                    </v-card>
+                  </v-col>
+                </v-row>
+              </v-card-text>
+            </v-tabs-window-item>
+
             <v-tabs-window-item v-if="canReadPermissions" value="permissions">
               <PermissionManager
                 v-if="loaded"
@@ -285,6 +523,42 @@ async function setProtection() {
     console.error(error);
   }
 }
+
+function copyViewJson() {
+  try {
+    const jsonString = JSON.stringify(view, null, 2);
+    functions.copyToClipboard(jsonString);
+  } catch (error) {
+    console.error('Failed to copy view JSON:', error);
+  }
+}
+
+// Helper functions for details tab
+function formatTimestamp(timestampMs: number): string {
+  if (!timestampMs) return '';
+  const date = new Date(timestampMs);
+  return date.toLocaleString();
+}
+
+function getCurrentViewVersion() {
+  if (!view.metadata.versions || view.metadata.versions.length === 0) return null;
+  return view.metadata.versions.find(
+    (version) => version['version-id'] === view.metadata['current-version-id'],
+  );
+}
+
+function getCurrentViewSchema() {
+  if (!view.metadata.schemas || view.metadata.schemas.length === 0) return null;
+  const currentVersion = getCurrentViewVersion();
+  if (!currentVersion) return null;
+  return view.metadata.schemas.find(
+    (schema) => schema['schema-id'] === currentVersion['schema-id'],
+  );
+}
+
+// Computed properties for safe access
+const currentViewVersion = computed(() => getCurrentViewVersion());
+const currentViewSchema = computed(() => getCurrentViewSchema());
 </script>
 
 <style scoped>
