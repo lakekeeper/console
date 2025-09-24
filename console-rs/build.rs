4f8a33904be8a86158311fe9c259847e61c256b3
Christian Thiel <christian@hansetag.com>
2025-09-24 19:48:46 +0200

    separate node root from assets directory

--- console-rs/build.rs
+++ console-rs/build.rs
@@ -6,16 +6,17 @@ fn main() {
     let out_dir = env::var_os("OUT_DIR").unwrap();
     let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
     let repo_dir = Path::new(&manifest_dir).parent().unwrap();
-    let node_dir = Path::new(&out_dir).join("node/dist");
+    let node_root = Path::new(&out_dir).join("node");
+    let asset_dir = node_root.join("dist");
 
-    println!("Node dir: {node_dir:?}");
-    println!("Repo dir: {repo_dir:?}");
+    println!("Node root: {node_root:?}");
+    println!("Asset dir (dist): {asset_dir:?}");
     // Copy everything from repo_dir to node_dir, we are not allowed to write anywhere else
-    fs::remove_dir_all(&node_dir).ok();
-    fs::create_dir_all(&node_dir).unwrap();
+    fs::remove_dir_all(&node_root).ok();
+    fs::create_dir_all(&asset_dir).unwrap();
     fs_extra::dir::copy(
         repo_dir,
-        &node_dir,
+        &node_root,
         &fs_extra::dir::CopyOptions::new().content_only(true),
     )
     .unwrap();
@@ -24,15 +25,15 @@ fn main() {
         .arg("-c")
         .arg(format!(
             "cd {} && HOME=\"{}\" npm ci",
-            node_dir.to_str().unwrap(),
-            node_dir.to_str().unwrap()
+            node_root.to_str().unwrap(),
+            node_root.to_str().unwrap()
         ))
-        .current_dir(node_dir.clone())
+        .current_dir(node_root.clone())
         .status()
         .expect("Failed to install Lakekeeper UI dependencies with npm");
     std::process::Command::new("npm")
         .args(["run", "build-placeholder"])
-        .current_dir(node_dir.clone())
+        .current_dir(node_root.clone())
         .status()
         .expect("Failed to build Lakekeeper UI with npm");
 }
