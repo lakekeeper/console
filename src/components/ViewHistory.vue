d3420be594fabf3e06af9de00efec3ce1c81815a
Viktor <vikkes@gmail.com>
2025-09-24 20:58:49 +0200

    fix any

--- src/components/ViewHistory.vue
+++ src/components/ViewHistory.vue
@@ -275,6 +275,7 @@
 <script setup lang="ts">
 import { computed, reactive, onMounted } from 'vue';
 import { useFunctions } from '../plugins/functions';
+import type { LoadViewResultReadable } from '@/gen/iceberg';
 
 // Types
 interface TimelineEvent {
@@ -287,7 +288,7 @@ interface TimelineEvent {
 
 // Props
 interface Props {
-  view: any;
+  view: LoadViewResultReadable;
 }
 
 const props = defineProps<Props>();
