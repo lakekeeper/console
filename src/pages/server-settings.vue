71337ec32fbf837ab0aaef5de04352ef2e42d572
Viktor <vikkes@gmail.com>
2025-10-03 10:41:11 +0200

    server page

--- src/pages/server-settings.vue
+++ src/pages/server-settings.vue
@@ -1,86 +1,73 @@
 <template>
   <v-tabs v-model="tab">
     <v-tab value="overview">overview</v-tab>
-    <v-tab
-      v-if="canReadAssignments && enabledAuthentication && enabledPermissions"
-      value="permissions">
-      Permissions
-    </v-tab>
-    <v-tab v-if="canListUsers && enabledAuthentication" value="users">users</v-tab>
+    <v-tab v-if="showPermissionsTab" value="permissions">Permissions</v-tab>
+    <v-tab v-if="showUsersTab" value="users">users</v-tab>
   </v-tabs>
   <v-tabs-window v-model="tab">
     <v-tabs-window-item value="overview">
-      <v-card>
-        <ServerOverview />
-      </v-card>
+      <ServerOverview />
     </v-tabs-window-item>
-    <v-tabs-window-item
-      v-if="canReadAssignments && enabledAuthentication && enabledPermissions"
-      value="permissions">
-      <v-card>
-        <PermissionManager
-          v-if="enabledPermissions"
-          :assignable-obj="permissionObject"
-          :relation-type="permissionType" />
-      </v-card>
+    <v-tabs-window-item v-if="showPermissionsTab" value="permissions">
+      <PermissionManager :assignable-obj="permissionObject" :relation-type="permissionType" />
     </v-tabs-window-item>
-    <v-tabs-window-item v-if="canListUsers && enabledAuthentication" value="users">
-      <v-card>
-        <UserManager :can-delete-users="canDeleteUsers" :can-list-users="canListUsers" />
-      </v-card>
+    <v-tabs-window-item v-if="showUsersTab" value="users">
+      <UserManager :can-delete-users="canDeleteUsers" :can-list-users="canListUsers" />
     </v-tabs-window-item>
   </v-tabs-window>
 </template>
 
 <script lang="ts" setup>
-import { useVisualStore } from '@/stores/visual';
 import { useFunctions } from '@/plugins/functions';
 import { computed, onMounted, reactive, ref } from 'vue';
 import { ServerAction } from '@/gen/management/types.gen';
 import { RelationType } from '@/common/interfaces';
 import { enabledAuthentication, enabledPermissions } from '@/app.config';
-import UserManager from '@/components/UserManager.vue';
-import ServerOverview from '@/components/ServerOverview.vue';
 
 const tab = ref('overview');
-const visual = useVisualStore();
 const functions = useFunctions();
-const permissionType = ref<RelationType>('server');
 
-const permissionObject = reactive<any>({
+const permissionObject = reactive<{
+  id: string;
+  name: string;
+}>({
   id: '',
-  description: '',
   name: 'Server',
 });
+
 const myAccess = reactive<ServerAction[]>([]);
 const canReadAssignments = ref(false);
 const canListUsers = ref(false);
 const canDeleteUsers = ref(false);
 
-const projectInfo = computed(() => {
-  return visual.getServerInfo();
-});
+// Computed properties for better performance and readability
+const permissionType = computed<RelationType>(() => 'server');
+
+const showPermissionsTab = computed(
+  () => canReadAssignments.value && enabledAuthentication && enabledPermissions,
+);
+
+const showUsersTab = computed(() => canListUsers.value && enabledAuthentication);
 
 async function init() {
-  await functions.getServerInfo();
-  permissionObject.id = projectInfo.value['server-id'];
+  const serverInfo = await functions.getServerInfo();
+  permissionObject.id = serverInfo['server-id'];
   await getMyAccess();
 }
 
 async function getMyAccess() {
   try {
     myAccess.splice(0, myAccess.length);
-    Object.assign(myAccess, await functions.getServerAccess());
-    checkPermission();
+    const serverAccess = await functions.getServerAccess();
+    myAccess.push(...serverAccess);
+
+    // Set permissions in one go
+    canReadAssignments.value = myAccess.includes('read_assignments');
+    canListUsers.value = myAccess.includes('list_users');
+    canDeleteUsers.value = myAccess.includes('delete_users');
   } catch (error) {
     console.error(error);
   }
-}
-
-function checkPermission() {
-  canReadAssignments.value = !!myAccess.includes('read_assignments');
-  canListUsers.value = !!myAccess.includes('list_users');
-  canDeleteUsers.value = !!myAccess.includes('delete_users');
 }
 
 onMounted(async () => {
